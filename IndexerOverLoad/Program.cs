454553e41d6276d654402d14643e732c1cdbc8f1
Administrator <Administrator@ADC-1>
2017-06-03 18:08:15 +0530

    Helper class created

--- IndexerOverLoad/Program.cs
+++ IndexerOverLoad/Program.cs
@@ -6,10 +6,72 @@ using System.Threading.Tasks;
 
 namespace IndexerOverLoad
 {
+    class IndererDemo
+        {
+        int[] a;
+        public int length;
+        public bool errflag;
+
+       private bool ISValid(int index)
+        {
+                if(index >= 0 & index <length)
+            {
+                return true;
+            }
+                else
+            {
+                return false;
+            }
+        }
+        
+        public IndererDemo (int size)
+        {
+            a = new int[size];
+            length = size;
+        }
+
+        public int this[int index]
+            {
+
+            get
+            {
+                if(ISValid(index))
+                {
+                    errflag = false;
+                    return a[index];
+
+                }
+                else
+                {
+                    errflag = true;
+                    return 0;
+                }
+            }
+
+            set
+            {
+                if (ISValid(index))
+                {
+                    errflag = false;
+                    a[index] = value;
+
+                }
+                else
+                {
+                    errflag = true;
+                    
+                }
+            }
+            }
+
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+
+            
         }
     }
 }
